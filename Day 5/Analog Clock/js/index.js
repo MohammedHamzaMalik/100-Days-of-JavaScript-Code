b84ace98e9fda90c85a567c0cce4c37fca6ce1d7
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-01-18 23:34:15 +0530

    Add files via upload

---
+++ Day 5/Analog Clock/js/index.js
@@ -0,0 +1,31 @@
+// const degree = 6;
+// const hr = document.querySelector("#hr");
+// const min = document.querySelector("#min");
+// const sec = document.querySelector("#sec");
+
+//     setInterval(() => {
+//         let day = new Date();
+//         let hh = day.getHours() * 30;
+//         let mm = day.getMinutes() * degree;
+//         let ss = day.getSeconds() * degree;
+
+//         hr.style.transform = `rotateZ(${(hh) + (mm/12)}degree)`;
+//         min.style.transform = `rotateZ(${mm}degree)`;
+//         sec.style.transform = `rotateZ(${ss}degree)`;
+//     })
+const deg = 6;
+     const hr = document.querySelector('#hr');
+     const min = document.querySelector('#min');
+     const sec = document.querySelector('#sec');
+     
+     setInterval(() => {
+     let day = new Date();
+     let hh = day.getHours() * 30;
+     let mm = day.getMinutes() * deg;
+     let ss = day.getSeconds() * deg;
+     
+     hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+     min.style.transform = `rotateZ(${mm}deg)`;
+     sec.style.transform = `rotateZ(${ss}deg)`;
+     
+     })
