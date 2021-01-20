55159c1534a2019e1f9c929b922d568cbe0d0bf1
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-01-20 23:07:16 +0530

    Add files via upload

---
+++ Day 7/Expand Cards/js/app.js
@@ -0,0 +1,19 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach(panel => {
+    panel.addEventListener('click', ()=> {
+        removingActiveClass();
+        panel.classList.add('active');
+    })
+})
+
+function removingActiveClass() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
+}
+
+var selector = document.body;
+function modeChanger() {
+    selector.classList.toggle('dark-mode');
+}
