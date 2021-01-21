b90d8bb374d57936fb7c7891c0bb8aa1ce9a6952
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-01-21 23:47:41 +0530

    Update app.js

--- Day 8/Newsletter PopUp/js/app.js
+++ Day 8/Newsletter PopUp/js/app.js
@@ -1,3 +1,4 @@
+// making a function which will toggle the class
 function toggleNewsletter(){
     const newsletter = document.getElementById("newsletter");
     newsletter.classList.toggle("active");
