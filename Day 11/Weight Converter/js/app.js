7fb09e3ad52db07502a77c69dfe2495fe2ba702f
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-01-24 23:31:54 +0530

    Update app.js

--- Day 11/Weight Converter/js/app.js
+++ Day 11/Weight Converter/js/app.js
@@ -1,16 +1,18 @@
-
+// selecting the form tag
 const form = document.querySelector("form");
 
+// adding event listener
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     const input = document.querySelector("input");
     var convertedWeight = document.querySelector("span");
     var kgTOpound;
-
+//     error if the input value is less than zero or it is not a number
     if ((isNaN(input.value)) || input.value <= 0) {
         convertedWeight.classList.add("error");
         convertedWeight.innerHTML = "<p>Please enter a valid number!</p>";
-
+        
+//         removing the class after specific interval of time
         setTimeout(() => {
             convertedWeight.innerHTML = "";
             convertedWeight.classList.remove("error");
