3953fc6ff37714c968be355c15ccc14357ede7d9
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-02-02 23:48:12 +0530

    Update script.js

--- Day 20/CSS Changer/js/script.js
+++ Day 20/CSS Changer/js/script.js
@@ -10,3 +10,9 @@ function update() {
     this.value + suffix
   );
 }
+// - CSS Variables - https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_custom_properties
+//       Updating CSS with JavaScript
+//     - CSS filter property - https://developer.mozilla.org/en-US/docs/Web/CSS/filter
+//     - HTMLElement.dataset - https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset - Takes everything on the data-'set' and puts in an object.
+//     - Element: mousemove event - https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
+//     -document.documentElement selects the entire HTML element
