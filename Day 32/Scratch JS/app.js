bb5d35ef31807fe9afe46099fee7d7ae089d56e0
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-02-14 16:21:34 +0530

    Update app.js

--- Day 32/Scratch JS/app.js
+++ Day 32/Scratch JS/app.js
@@ -1,5 +1,6 @@
 document.addEventListener("mousemove", (e) => {
   const body = document.querySelector("body");
+//   creating span element for scratching 
   const scratcher = document.createElement("span");
   scratcher.style.left = -75 + e.offsetX + "px";
   scratcher.style.top = -75 + e.offsetY + "px";
