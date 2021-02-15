dd896de2076e96a078f07254aed578497871b5c8
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-02-15 18:31:28 +0530

    Update app.js

--- Day 33/Accordion/app.js
+++ Day 33/Accordion/app.js
@@ -2,6 +2,7 @@ const accordion = document.getElementsByClassName("accordion");
 
 for (let i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function () {
+//     toggling the active class in each accrodion
     this.classList.toggle("active");
   });
 }
