c8682b57f96707a5e6cf59e4b2ff3c2be14ea82b
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-02-16 23:25:30 +0530

    Update app.js

--- Day 34/Parallax Effect/app.js
+++ Day 34/Parallax Effect/app.js
@@ -1,11 +1,13 @@
 document.addEventListener("mousemove", parallax);
 var layers = document.querySelectorAll(".layer");
 
+// creating function for parallax effect
 function parallax(e) {
   layers.forEach((layer) => {
-    var speed = layer.getAttribute("data-speed");
-    var x = (window.innerWidth - e.pageX * speed) / 250;
-    var y = (window.innerWidth - e.pageX * speed) / 250;
+    var movement = layer.getAttribute("data-speed");
+//     controlling the movement of image while moving the mouse
+    var x = (window.innerWidth - e.pageX * movement) / 250;
+    var y = (window.innerWidth - e.pageX * movement) / 250;
 
     layer.style.transform = `translate(${-x}px) translateY(${y}px)`;
   });
