c177b14fd1903c1ae39ec4d91fb79fe1ea385ef6
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-02-17 17:09:10 +0530

    Update app.js

--- Day 35/SVG Animation/app.js
+++ Day 35/SVG Animation/app.js
@@ -2,9 +2,10 @@ const svgPath = document.querySelectorAll("#svg path");
 
 for (let i = 0; i < svgPath.length; i++) {
     console.log(`Letter ${i} is ${svgPath[i].getTotalLength()}`)
-
+//     this will give the length of every alphabet
 }
 
+// tried this but it will require only one path and I am having 5 path actually 10 😅
 // anime({
 //   targets: "#svg path",
 //   strokeDashoffset: [anime.setDashoffset, 0],
