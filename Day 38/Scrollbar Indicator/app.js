8f7e24c2aa6453713a4ef6c4dae2dbbbbc5a612f
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-02-20 17:18:16 +0530

    Update app.js

--- Day 38/Scrollbar Indicator/app.js
+++ Day 38/Scrollbar Indicator/app.js
@@ -1,6 +1,7 @@
 const scrollIndi = document.getElementById("scrollIndicator");
 const height = window.document.body.scrollHeight - window.innerHeight;
 
+// adding scroll eventlistener 
 window.addEventListener("scroll", () => {
   {
     const percent = (window.scrollY / height) * 100;
