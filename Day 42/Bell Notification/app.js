da3002a305b66bc072caf0de03ca3853cc95251d
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-02-24 22:37:06 +0530

    Update app.js

--- Day 42/Bell Notification/app.js
+++ Day 42/Bell Notification/app.js
@@ -4,6 +4,7 @@ const notify = document.getElementById("notification");
 button.addEventListener("click", () => {
   const counter = Number(notify.getAttribute("data-count")) || 0;
 
+//   adding count whenever clicked
   notify.setAttribute("data-count", counter + 1);
   notify.classList.add("count");
   notify.classList.add("notify");
