206a42c4d50844b81ca3a916db7971e109136cec
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-02-27 15:34:12 +0530

    Add files via upload

---
+++ Day 45/Isometric Words/app.js
@@ -0,0 +1,7 @@
+var words = document.getElementById("words");
+var isometric = "";
+
+for (let i = 0; i < 20; i++) {
+  isometric += (isometric ? "," : " ") + -i * 1 + "px " + i * 1 + "px 0 #000";
+}
+words.style.textShadow = isometric;
