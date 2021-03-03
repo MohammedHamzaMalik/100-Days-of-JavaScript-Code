2809d5924d6a2c1d4eb56b3b1ef2656a172dfd8c
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-03-03 16:36:45 +0530

    Update app.js

--- Day 49/Scrolling Images/app.js
+++ Day 49/Scrolling Images/app.js
@@ -4,13 +4,17 @@ window.addEventListener("scroll", animateCards);
 animateCards();
 
 function animateCards() {
+//   checking the bottom 
   const checkingBottom = (window.innerHeight / 4) * (125 / 25);
   card.forEach((singleCard) => {
     const cardTop = singleCard.getBoundingClientRect().top;
     var toSlideIn = cardTop < checkingBottom;
+//     if we slide below the bottom level this will check
     if (toSlideIn) {
+//       it will add our new card 
       singleCard.classList.add("slidingIn");
     } else {
+//       this will remove the added card
       singleCard.classList.remove("slidingIn");
     }
   });
