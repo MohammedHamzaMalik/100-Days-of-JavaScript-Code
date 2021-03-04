867069a4b3e643dccfc2ec0d51b687597e876032
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-03-04 17:01:52 +0530

    Update app.js

--- Day 50/Rotating Animation/app.js
+++ Day 50/Rotating Animation/app.js
@@ -1,12 +1,15 @@
 const squares = document.querySelectorAll(".squares");
 
 squares.forEach((square, index) => {
+//   increasing the width of the border
   square.style.borderWidth = (index + 1) * 4 + "px";
+//   adding animation to all the squares
   square.style.animationName = `rotate-${index}`;
 
   const degree = (index + 1) * 360 * 2;
 
   const animation = document.createElement("style");
+//   styling the newly made animation to all the squares
   animation.innerHTML = `@keyframes rotate-${index} {
         to {
             transform: translate(-50%,-50%) rotate(${degree}deg);
