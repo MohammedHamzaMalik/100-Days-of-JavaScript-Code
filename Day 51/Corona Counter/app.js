0ab9c6ae87616a39fca82655871a04cfe3f5d62c
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-03-05 16:30:24 +0530

    Update app.js

--- Day 51/Corona Counter/app.js
+++ Day 51/Corona Counter/app.js
@@ -1,14 +1,17 @@
 const counter = document.querySelectorAll(".counter");
 
-// counting();
-
+// counting in each counter
 counter.forEach((singleCounter) => {
+//   initializing each counter to 0
   singleCounter.innerText = 0;
 
   const counting = () => {
+//     assigning the value of total
     const totalCount = +singleCounter.getAttribute("data-counter");
+    
     const incrementing = +singleCounter.innerText;
 
+//     controlling the speed of the counter
     const speed = totalCount / 1000;
 
     if (incrementing < totalCount) {
