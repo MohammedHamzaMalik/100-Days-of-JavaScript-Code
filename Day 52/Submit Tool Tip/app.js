7fb4b3038f5c1fe1f48cbf78c7d05bce8b07f622
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-03-06 18:02:29 +0530

    Add files via upload

---
+++ Day 52/Submit Tool Tip/app.js
@@ -0,0 +1,12 @@
+    const button = document.querySelector(".submit-btn");
+    const body = document.body;
+
+    button.addEventListener("click", ()=> {
+        button.innerText = "Submitted";
+        button.style.backgroundColor = "#81FF00";
+        button.style.color = "#333";
+        button.style.animation = "animate 0.5s ease"
+
+        body.style.backgroundColor = "#7e00ff";
+        
+    })
